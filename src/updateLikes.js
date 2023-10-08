83e3774dadad6fee3e7fb5f56241195b9c3d49da
Jozefpv <jozefpv99@gmail.com>
2023-10-08 19:02:57 +0100

    db config

--- src/updateLikes.js
+++ src/updateLikes.js
@@ -1,18 +1,6 @@
 const mongoose = require('mongoose');
 const { Apartamento } = require('./models/apartamento'); // Importa el modelo de Mongoose
 
-const URI = 'mongodb+srv://jozef:jozef@cluster0.trrjhin.mongodb.net/apartamentosDB?retryWrites=true&w=majority'
-
-mongoose.connect(URI)
-
-mongoose.connection.on('open', () =>{ 
-    console.log('Base de datos conectada')
-})
-
-mongoose.connection.on('error', (err) =>{ 
-    console.log(err)
-})
-
 Apartamento.updateMany({}, { $set: { likes: 0 } })
   .then((res) => {
     console.log("Se han actualizado los apartamentos exitosamente.");
